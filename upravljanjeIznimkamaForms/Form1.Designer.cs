372f6f9300b778860653d19283b7e8eb41f83ce0
Lovro Šverko <lovro.sverko@gmail.com>
2023-10-18 09:01:29 +0200

    Add files via upload

---
+++ upravljanjeIznimkamaForms/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace upravljanjeIznimkamaForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUnos = new System.Windows.Forms.TextBox();
+            this.btnUnesi = new System.Windows.Forms.Button();
+            this.txtIspis = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtUnos
+            // 
+            this.txtUnos.Location = new System.Drawing.Point(138, 139);
+            this.txtUnos.Name = "txtUnos";
+            this.txtUnos.Size = new System.Drawing.Size(310, 26);
+            this.txtUnos.TabIndex = 0;
+            // 
+            // btnUnesi
+            // 
+            this.btnUnesi.Location = new System.Drawing.Point(476, 142);
+            this.btnUnesi.Name = "btnUnesi";
+            this.btnUnesi.Size = new System.Drawing.Size(144, 98);
+            this.btnUnesi.TabIndex = 1;
+            this.btnUnesi.Text = "Unesi";
+            this.btnUnesi.UseVisualStyleBackColor = true;
+            this.btnUnesi.Click += new System.EventHandler(this.btnUnesi_Click);
+            // 
+            // txtIspis
+            // 
+            this.txtIspis.Location = new System.Drawing.Point(138, 214);
+            this.txtIspis.Name = "txtIspis";
+            this.txtIspis.Size = new System.Drawing.Size(310, 26);
+            this.txtIspis.TabIndex = 2;
+            this.txtIspis.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Unesi svoju godinu rođenja";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Rođen si:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIspis);
+            this.Controls.Add(this.btnUnesi);
+            this.Controls.Add(this.txtUnos);
+            this.Name = "Form1";
+            this.Text = "Upravljanje Iznimkama";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUnos;
+        private System.Windows.Forms.Button btnUnesi;
+        private System.Windows.Forms.TextBox txtIspis;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ upravljanjeIznimkamaForms/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace upravljanjeIznimkamaForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUnesi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int godRod = Convert.ToInt16(txtUnos.Text);
+
+                txtIspis.Text = godRod.ToString();
+                txtUnos.Clear();
+            }
+            catch(Exception greska)
+            {
+                DialogResult odabir = MessageBox.Show("Molim Vas da upišete broj." +
+                    "\nDa li želite izbrisati Vaš postojeći unos?",
+                    "Pogrešan unos", MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Error);
+
+                switch(odabir)
+                {
+                    case DialogResult.Yes:
+                        txtUnos.Clear() ; break;
+                    case DialogResult.No:
+                        break;
+                }
+
+                txtUnos.Select();
+            }
+
+        }
+    }
+}
